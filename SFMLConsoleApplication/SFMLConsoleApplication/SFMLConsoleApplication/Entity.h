602e891aa87e578a658ec5ebfcf830d10c18805f
MattssonHubGit <d16erima@student.his.se>
2019-09-10 15:38:28 +0200

    Initial project

---
+++ SFMLConsoleApplication/SFMLConsoleApplication/SFMLConsoleApplication/Entity.h
@@ -0,0 +1,9 @@
+#ifndef ENTITIY_H_
+#define ENTITIY_H_
+
+#include "SFML/Graphics.hpp"
+
+
+
+
+#endif // !ENTITIY_H_
---
+++ SFMLConsoleApplication/SFMLConsoleApplication/SFMLConsoleApplication/Main.cpp
@@ -0,0 +1,26 @@
+#include "SFML/Graphics.hpp"
+#include <string>
+
+using namespace sf;
+using namespace std;
+
+
+int main(){
+	const string windowTitle = "SFML Console Application";
+	const VideoMode videoMode = VideoMode(768, 1024);
+	const Color backgroundColor = Color::Black;
+	const unsigned int FRAMERATE_LIMIT = 60;
+	RenderWindow window(videoMode, windowTitle, Style::Titlebar | Style::Close);
+	window.setFramerateLimit(FRAMERATE_LIMIT);
+	while(window.isOpen()) {
+		Event event;
+		while(window.pollEvent(event)){
+			if(event.type == Event::Closed){
+				window.close();
+			}
+		}
+		window.clear(backgroundColor);
+		window.display();
+	}
+	return 0;
+}
